dfc0ae9ce46a118ceeba89f7e5c8500e9f1ba984
11249a020 <11249a020@kanchiuniv.ac.in>
2026-04-19 16:45:34 +0530

    Update 11249a020-exp9.c

--- 11249a020-exp9.c
+++ 11249a020-exp9.c
@@ -4,56 +4,58 @@
 
 void swap(int* a, int* b) {
     int t = *a;
-        *a = *b;
-            *b = t;
-            }
+    *a = *b;
+    *b = t;
+}
 
-            int partition(int arr[], int low, int high) {
-                int pivot = arr[high];
-                    int i = low;
-                        for (int j = low; j < high; j++) {
-                                if (arr[j] <= pivot) {
-                                            swap(&arr[i], &arr[j]);
-                                                        i++;
-                                                                }
-                                                                    }
-                                                                        swap(&arr[i], &arr[high]);
-                                                                            return i;
-                                                                            }
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
 
-                                                                            int randomized_partition(int arr[], int low, int high) {
-                                                                                int r = low + rand() % (high - low + 1);
-                                                                                    swap(&arr[r], &arr[high]);
-                                                                                        return partition(arr, low, high);
-                                                                                        }
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
 
-                                                                                        void quicksort(int arr[], int low, int high) {
-                                                                                            if (low < high) {
-                                                                                                    int p = randomized_partition(arr, low, high);
-                                                                                                            quicksort(arr, low, p - 1);
-                                                                                                                    quicksort(arr, p + 1, high);
-                                                                                                                        }
-                                                                                                                        }
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
 
-                                                                                                                        void printArray(int arr[], int size) {
-                                                                                                                            for (int i = 0; i < size; i++)
-                                                                                                                                    printf("%d ", arr[i]);
-                                                                                                                                        printf("\n");
-                                                                                                                                        }
+int randomized_partition(int arr[], int low, int high) {
+    int r = low + rand() % (high - low + 1);
+    swap(&arr[r], &arr[high]);
+    return partition(arr, low, high);
+}
 
-                                                                                                                                        int main() {
-                                                                                                                                            srand((unsigned int)time(NULL));
-                                                                                                                                                int arr[] = { 6, 4, 12, 8, 15, 16 };
-                                                                                                                                                    int n = sizeof(arr) / sizeof(arr[0]);
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int p = randomized_partition(arr, low, high);
+        quicksort(arr, low, p - 1);
+        quicksort(arr, p + 1, high);
+    }
+}
 
-                                                                                                                                                        printf("Original array: ");
-                                                                                                                                                            printArray(arr, n);
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 
-                                                                                                                                                                quicksort(arr, 0, n - 1);
+int main() {
+    srand((unsigned int)time(NULL));
 
-                                                                                                                                                                    printf("Sorted array: ");
-                                                                                                                                                                        printArray(arr, n);
+    int arr[] = {6, 4, 12, 8, 15, 16};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-                                                                                                                                                                            return 0;
-                                                                                                                                                                            }
-                                                                                                                                                                            
+    printf("Original array: ");
+    printArray(arr, n);
+
+    quicksort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
